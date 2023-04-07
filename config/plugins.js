22234d58f22f310cfd90cee3486ba8b5977b8bf4
hungtd30 <hungtd30>
2023-04-07 08:18:32 +0700

    turn off redis

--- config/plugins.js
+++ config/plugins.js
@@ -13,40 +13,40 @@ module.exports = {
   "content-versioning": {
     enabled: true,
   },
-  redis: {
-    config: {
-      connections: {
-        default: {
-          connection: {
-            host: "127.0.0.1",
-            port: 6379,
-            db: 0,
-          },
-          settings: {
-            debug: false,
-          },
-        },
-      },
-    },
-  },
-  "rest-cache": {
-    config: {
-      provider: {
-        name: "redis",
-        options: {
-          max: 32767,
-          connection: "default",
-        },
-      },
-      strategy: {
-        // if you are using keyPrefix for your Redis, please add <keysPrefix>
-        keysPrefix: "<redis_keyPrefix>",
-        contentTypes: [
-          // list of Content-Types UID to cache
-        ],
-      },
-    },
-  },
+  // redis: {
+  //   config: {
+  //     connections: {
+  //       default: {
+  //         connection: {
+  //           host: "127.0.0.1",
+  //           port: 6379,
+  //           db: 0,
+  //         },
+  //         settings: {
+  //           debug: false,
+  //         },
+  //       },
+  //     },
+  //   },
+  // },
+  // "rest-cache": {
+  //   config: {
+  //     provider: {
+  //       name: "redis",
+  //       options: {
+  //         max: 32767,
+  //         connection: "default",
+  //       },
+  //     },
+  //     strategy: {
+  //       // if you are using keyPrefix for your Redis, please add <keysPrefix>
+  //       keysPrefix: "<redis_keyPrefix>",
+  //       contentTypes: [
+  //         // list of Content-Types UID to cache
+  //       ],
+  //     },
+  //   },
+  // },
   // ...
 
   "traking-appversion": {
